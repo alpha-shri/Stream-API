c98cf1b19dcc9878a662ff0a36c2684740c3e79a
Shrijeet Kumar Panda <shrijeetkumarpanda@gmail.com>
2021-03-25 11:28:45 +0530

    added BLANK_VALUE in StreamConstants.java

--- src/com/stream/constants/StreamConstants.java
+++ src/com/stream/constants/StreamConstants.java
@@ -3,5 +3,6 @@ package com.stream.constants;
 public class StreamConstants {
 	
 	public static final String MESSAGE = "Welcome to Stream API project";
+	public static final String BLANK_VALUE = "";
 
 }
